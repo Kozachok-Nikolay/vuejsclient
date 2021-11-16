4d0a16a925435203d59afbef5bf0a517d93c78c2
Николай Козачок <deraeser@bk.ru>
2021-11-16 23:11:35 +0300

    button!!!

---
+++ src/components/ChatPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <div id = "example-1">
+      <button v-on:click="counter += 1">+1</button>
+      <p>Кнопка выше была нажата {{counter}} раз</p>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+ data: () => {
+   return {
+     counter: 0,
+     username: '',
+     message: '',
+     messages: [],
+     intervalCtx: null,
+     LastMsgID: 0,
+
+   }
+ }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
