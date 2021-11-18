6cc51847b502ff4312d0a21b75721fcb078471fe
Николай Козачок <deraeser@bk.ru>
2021-11-18 20:07:28 +0300

    done

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,12 @@
 </template>
 
 <script>
-import ChatPage from './components/ChatPage.vue'
+import ChatPage from './components/ChatPage';
 export default {
   name: 'App',
   components: {
-    ChatPage
-  }
+    ChatPage,
+  },
+  data: () => ({}),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
